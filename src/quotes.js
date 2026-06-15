c9cdaa829492a9da574a9e55c1a339152cac86e0
Juanerros <juancottier0@gmail.com>
2026-06-15 16:17:33 -0300

    mod frases

--- src/quotes.js
+++ src/quotes.js
@@ -1,13 +1,13 @@
 // Frases aleatorias para acompañar a cada gatito.
 export const quotes = [
   { text: "Un regalito para vos, espero te guste <3", author: "toca el boton!" },
-  { text: "Sos la razón por la que todos los días se vuelven bonitos.", author: "Para vo" },
+  { text: "Sos la razón por la que todos los días se vuelven bonitos.", author: "Para vos" },
   { text: "Ojala te pase todo lo bueno del mundo, eme.", author: "Cuan" },
   { text: "<3", author: "Para vo" },
-  { text: "La vida es mas bonita con miaus cerca y con gente como vos.", author: "nose" },
+  { text: "La vida es mas bonita con miaus y alguien como vos cerca.", author: "nose" },
   { text: "Ola eme", author: "Chau eme" },
-  { text: "No importa qué tan ocupado este, siempre hay un rincón una parte mia pensando en vos.", author: "Cuam" },
-  // { text: "Sos pura ternura, igualź que estos michis.", author: "Anónimo" },
+  { text: "No importa qué tan ocupado este, siempre hay una parte mia pensando en vos.", author: "Cuam" },
+  { text: "Sos alguien espectacular emeee.", author: "yo" },
   { text: "Sos mi matriz inversa", author: "Dea que decia" },
   { text: "Te quiero eme", author: "ola" },
   { text: "Te extraño", author: "Mucho" },
@@ -15,15 +15,16 @@ export const quotes = [
   { text: "Se parece a vos", author: "jijij" },
   { text: "Sos mi mapa", author: "geopolitico" },
   { text: "Ya te comprare un diccionario", author: "de los baratos eh" },
+  { text: "Tontaaa", author: "digo guapa" },
+  { text: "Graciosa y bajita", author: "todo en uno" },
 ]
 
 export function frasePorIndice(indice, ultima) {
-  let aux;
   let frase;
   
   delete quotes[0];
 
-  while (aux == ultima) {
+  while (frase == ultima) {
     frase = quotes[indice % quotes.length]
   }
   return frase
